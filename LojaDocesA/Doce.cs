cf58ca0651b6c93aed1eb1fca6a498bf4d58bc3d
Daniel <danielcolombo2009@gmail.com>
2026-09-03 22:36:00 -0300

    feat: Criando a classe doces

---
+++ LojaDocesA/Doce.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LojaDocesA
+{
+    internal class Doce
+    {
+        //PROPRIEDADES (ATRIBUTOS)
+        public string Confeiteiro {  get; set; }
+
+    }
+}
